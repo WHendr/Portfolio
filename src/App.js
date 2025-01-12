c03baa14fbdec1ab32f4636fd5e5f27de95a1b71
Will H <81208850+WHendr@users.noreply.github.com>
2025-01-12 16:04:07 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Menu, X, Github, Linkedin, Mail, ExternalLink } from 'lucide-react';
 
 import { About } from './components/About.js'
+import { Contact } from './components/About.js'
+import { Home } from './components/About.js'
+import { Layout } from './components/About.js'
+import { Projects } from './components/About.js'
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState('home');
