1a450bfc770aea7d4cb7bdd26e508364d9bbfb97
Will H <81208850+WHendr@users.noreply.github.com>
2025-01-12 17:22:29 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,10 @@ const Home = () => (
     <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
       <div className="text-center">
         <h1 className="text-4xl font-extrabold text-white sm:text-5xl">
-          Your Name
+          William Hendrickson
         </h1>
         <p className="mt-4 text-xl text-gray-300">
-          Full Stack Developer
+          Developer with a masters in Software Engineer with a concentration in Real-Time and Game Systems
         </p>
         <div className="mt-8">
           <button
